eede8fc117b984d142974ba7c991039fa58cc1d0
Kaayathiri <kaayathiri_11@hotmail.co.uk>
2019-06-07 16:19:34 +0100

    buttons north east south west

--- windows7.java
+++ windows7.java
@@ -1,7 +1,17 @@
-import java.awt.Frame;
+import java.awt.*;
 class windows7{
 	public static void main(String xyz[]){
 		Frame win=new Frame();
+		Button B1=new Button("Click");
+		Button B2=new Button("GET");
+		Button B3=new Button("ME");
+		Button B4=new Button("HOME");
+		Button B5=new Button ("NOW");
+		win.add(B1, BorderLayout.CENTER);
+		win.add(B2, BorderLayout.NORTH);
+		win.add(B3, BorderLayout.EAST);
+		win.add(B4, BorderLayout.SOUTH);
+		win.add(B5, BorderLayout.WEST);
 		win.setSize(600,400);
 		win.setVisible(true);
 	}
